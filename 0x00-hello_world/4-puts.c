a9aae55fbde401f749328c8637f5693b2380bb6c
Kelvin12-bit <kelvinaryee89@gmail.com>
2022-10-13 14:20:56 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /*Display the input from user*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
