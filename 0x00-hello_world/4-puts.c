e5f522abde4a5443d9e5094ef01c3c11b92164fb
Kelvin12-bit <kelvinaryee89@gmail.com>
2022-10-13 14:17:46 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*Display the output*/
+/*Display the input from user*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
 
