6a21045948ada3684ceb0e8050381e7a42a1eb79
Kelvin12-bit <kelvinaryee89@gmail.com>
2022-10-14 16:43:01 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - User entry
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
+
