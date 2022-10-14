8c9dea48266403210b748e21551aecc2dd11e904
Kelvin12-bit <kelvinaryee89@gmail.com>
2022-10-14 15:49:20 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - User entry
+ * Return: 0
+ */
+int main(void)
+{
+char lower;
+for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+		putchar ('\n');
+	}
+				return (0);
+}
