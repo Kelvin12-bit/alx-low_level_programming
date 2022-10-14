0f680be369704651ff24c7c234df9c942e7404ae
Kelvin12-bit <kelvinaryee89@gmail.com>
2022-10-14 16:36:20 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,14 @@ int main(void)
 {
 char low;
 	for (low = 'a'; low <= 'z'; low++)
+	{
 		if (low == 'q' || low == 'e')
+		{
 			continue;
-		putchar(low);
-			putchar('\n');
+			putchar(low);
+		putchar ('\n');
+		}
+	}
 	return (0);
 }
 
