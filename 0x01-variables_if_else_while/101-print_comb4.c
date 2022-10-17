323c73d8ea4f0d0d0d56b63f99de6127a9872e8c
Kelvin12-bit <kelvinaryee89@gmail.com>
2022-10-17 13:58:18 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,21 +7,31 @@ int main(void)
 {
 int x, y, z;
 
-	for (x = 0; x < 8; x++)
+	for (x = 0; x <= 9; x++)
 	{
-	for (y = x + 1; y < 9; y++)
+	for (y = 0; y <= 9; y++)
 	{
-		for (z = y + 1; z < 10; z++)
+		for (z = 0; z <= 9; z++)
 		{
-			putchar((x % 10) + '0');
-			putchar((y % 10) + '0');
-			putchar((z % 10) + '0');
+			if (y < z && z < x)
+			{
+			putchar(x);
+			putchar(y);
+			putchar(z);
 			if (x == 7 && y == 8 && z == 9)
-				continue;
+			{
+				break;
+				}
+				else
+				{
 			putchar(',');
 			putchar(' ');
 		}
+
 	}
 	}
+	}
+	}
+	putchar ('\n');
 	return (0);
 }
